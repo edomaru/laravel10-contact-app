87e9b4166e46fdb29b4029e7c16d64de13844256
Eding Muhamad <masaruedo@yahoo.com>
2023-10-26 16:08:00 +0700

    Use dependeny injection in the controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\CompanyRepository;
 
 class ContactController extends Controller
 {
-    public function index()
+    public function __construct(protected CompanyRepository $company)
     {
-        $companies = [
-            1 => ['name' => 'Company One', 'contacts' => 3],
-            2 => ['name' => 'Company Two', 'contacts' => 5],
-        ];
+    }
+
+    public function index(CompanyRepository $company, Request $request)
+    {
+        // dd($request->sort_by);
+        // $companies = [
+        //     1 => ['name' => 'Company One', 'contacts' => 3],
+        //     2 => ['name' => 'Company Two', 'contacts' => 5],
+        // ];
+        $companies = $company->pluck();
         $contacts = $this->getContacts();
         return view('contacts.index', compact('contacts', 'companies'));
     }
