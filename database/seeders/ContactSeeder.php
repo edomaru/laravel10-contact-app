cfa62291aa5b2f0f4029cfc25ce685cadf13390e
Eding Muhamad <masaruedo@yahoo.com>
2023-10-27 09:19:57 +0700

    populate the contacts table with seeders

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 
 class ContactSeeder extends Seeder
 {
@@ -12,6 +15,25 @@ class ContactSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $companies = Company::all();
+        $faker = Faker::create();
+        $contacts = [];
+
+        foreach ($companies as $company) {
+            foreach (range(1, 5) as $index) {
+                $contact = [
+                    'first_name' => $faker->firstName(),
+                    'last_name' => $faker->lastName(),
+                    'phone' => $faker->phoneNumber(),
+                    'email' => $faker->email(),
+                    'address' => $faker->address(),
+                    'company_id' => $company->id,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+                $contacts[] = $contact;
+            }
+        }
+        Contact::insert($contacts);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CompanySeeder::class);
+        $this->call([
+            CompanySeeder::class,
+            ContactSeeder::class,
+        ]);
     }
 }
