b7b479807a3e613d547b6cd7e8c6532005deaba9
Eding Muhamad <masaruedo@yahoo.com>
2023-10-28 15:26:16 +0700

    Protect routes from unauthenticated users

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // public function __construct()
+    // {
+    //     $this->middleware(['auth']);
+    // }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('dashboard');
+    }
+}
